81769fd4561931b2a33bdaeea526c5144b656486
Hasan Hüseyin Şenay <hasanhsenay@proton.me>
2026-01-04 00:50:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -9,24 +9,6 @@ const userNameEl = document.querySelector("#userName");
 const userEmailEl = document.querySelector("#userEmail");
 const userProfilePictureEl = document.querySelector("#pp");
 
-
-/*const firebaseConfig = {
-    apiKey: "AIzaSyDJNv0oR8idDU5ivkeetoaN6HevE9K_rIw",
-    authDomain: "librasave-e16e4.firebaseapp.com",
-    projectId: "librasave-e16e4",
-    storageBucket: "librasave-e16e4.firebasestorage.app",
-    messagingSenderId: "500379972592",
-    appId: "1:500379972592:web:3cb61d897a975c21392335",
-    measurementId: "G-VKFVV3MS51"
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};*/
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app); 
