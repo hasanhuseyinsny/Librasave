d8b479ac21453bba649e85e1f3894fcc235978ed
Hasan Hüseyin Şenay <hasanhsenay@proton.me>
2026-01-04 00:58:17 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDJNv0oR8idDU5ivkeetoaN6HevE9K_rIw",
+    authDomain: "librasave-e16e4.firebaseapp.com",
+    projectId: "librasave-e16e4",
+    storageBucket: "librasave-e16e4.firebasestorage.app",
+    messagingSenderId: "500379972592",
+    appId: "1:500379972592:web:3cb61d897a975c21392335",
+    measurementId: "G-VKFVV3MS51"
+};
